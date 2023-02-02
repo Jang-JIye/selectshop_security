1061522c04321210f0cbbf96039a1b1882569b6b
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-02-02 14:30:32 +0900

    #2 [feat] 카카오에서 보내주는 '인가코드' 처리
    UserController : kakaoService 의존성 주입

--- myselectshop/src/main/java/com/sparta/myselectshop/controller/UserController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.sparta.myselectshop.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.myselectshop.dto.LoginRequestDto;
 import com.sparta.myselectshop.dto.SignupRequestDto;
+import com.sparta.myselectshop.jwt.JwtUtil;
+import com.sparta.myselectshop.service.KakaoService;
 import com.sparta.myselectshop.service.UserService;
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class UserController {
 
     private final UserService userService;
+    private final KakaoService kakaoService;
 
     @GetMapping("/signup")
     public ModelAndView signupPage() {
@@ -49,6 +54,20 @@ public class UserController {
     @PostMapping("/forbidden")
     public ModelAndView postForbidden() {
         return new ModelAndView("forbidden");
+    }
+
+    //카카오에서 보내주는 '인가코드' 처리
+    @GetMapping("/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        // code : 카카오 서버로부터 받은 인가 코드
+        String createToken = kakaoService.kakaoLogin(code, response);
+
+        //Cookie 생성 및 직접 브라우저에 set
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "redirect:/api/shop";
     }
 
 }
