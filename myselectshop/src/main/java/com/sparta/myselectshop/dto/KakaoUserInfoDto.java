31591ae04da76af47f5f7bf9585a7e9db2307d5b
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-02-02 16:55:07 +0900

    #2 [chore] 자잘한 수정
    login.html : 문장 삭제
    KakaoUserInfoDto :  오타 수정

--- myselectshop/src/main/java/com/sparta/myselectshop/dto/KakaoUserInfoDto.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/dto/KakaoUserInfoDto.java
@@ -7,13 +7,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 //AccessToken 을 사용해서 카카오 서버에서 사용자 정보를 가져옴 -> 가지고 온 정보를 담아두기 위해 KakaoUserInfoDto 추가
 public class KakaoUserInfoDto {
-    private String nicknmae;
+    private String nickname;
     private Long id;
     private String email;
 
-    public KakaoUserInfoDto(Long id, String nicknmae, String email) {
+    public KakaoUserInfoDto(Long id, String nickname, String email) {
         this.id = id;
-        this.nicknmae = nicknmae;
+        this.nickname = nickname;
         this.email = email;
     }
 }
