53c4a6447ef5654312004f7f43983f3dd0c4a0e0
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-02-05 20:00:41 +0900

    #4 [feature] Spring AOP

--- myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,8 +1,9 @@
 package com.sparta.myselectshop.controller;
 
-import com.sparta.myselectshop.dto.*;
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
-import com.sparta.myselectshop.entity.UserRoleEnum;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 @RequestMapping("/api")
@@ -21,7 +20,7 @@ public class ProductController {
     private final ProductService productService;
 
     // 관심 상품 등록하기
-  //  @Secured(UserRoleEnum.Authority.ADMIN)
+    //@Secured(UserRoleEnum.Authority.ADMIN)
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 응답 보내기
@@ -36,9 +35,9 @@ public class ProductController {
             @RequestParam("sortBy") String sortBy,
             @RequestParam("isAsc") boolean isAsc,
             @AuthenticationPrincipal UserDetailsImpl userDetails
-            ) {
-        //응답 보내기
-        return productService.getProducts(userDetails.getUser(), page -1, size, sortBy,isAsc);
+    ) {
+        // 응답 보내기
+        return productService.getProducts(userDetails.getUser(), page-1, size, sortBy, isAsc);
     }
 
     // 관심 상품 최저가 등록하기
@@ -47,9 +46,13 @@ public class ProductController {
         // 응답 보내기 (업데이트된 상품 id)
         return productService.updateProduct(id, requestDto, userDetails.getUser());
     }
+
     // 상품에 폴더 추가
     @PostMapping("/products/{productId}/folder")
-    public Long addFolder(@PathVariable Long productId, @RequestParam Long folderId, @AuthenticationPrincipal UserDetailsImpl userDetails
+    public Long addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
         Product product = productService.addFolder(productId, folderId, userDetails.getUser());
         return product.getId();
